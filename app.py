4b6ed21c396bbd6e56c4b17adad7c7af382244f3
LukeYaxley <lukeyaxley21@gmail.com>
2024-01-12 16:36:22 +0000

    added users

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import json
 
+import firebase_admin
+from firebase_admin import auth
 from firebase import firebase
 from flask import Flask,render_template,request
-
+import pyrebase
 app = Flask(__name__)
+default_app = firebase_admin.initialize_app()
 
 firebase = firebase.FirebaseApplication('https://gevs-e6e1e-default-rtdb.firebaseio.com/', None)
-auth = firebase.authentication
+authentication = firebase.authentication
 
 @app.route('/')
 def index():  # put application's code here
@@ -21,8 +24,18 @@ def register():
         Name = request.form.get("Name")
         Const = request.form.get("Const")
         Birthday = request.form.get("Birthday")
-        jsonfile = {"UVC":UVC,"Name":Name,"Const":Const,"Birthday":Birthday}
+        VoterID = request.form.get("VoterID")
+        Password = request.form.get("Password")
+        jsonfile = {"UVC":UVC,"Name":Name,"Const":Const,"Birthday":Birthday,"VoterID":VoterID}
         firebase.post("/Voters",jsonfile)
+        #create user in firebase
+
+        auth.create_user(
+            email=VoterID,
+            email_verified=False,
+            password=Password,
+            display_name=Name
+            )
         return "Your name is " + Name + UVC +Birthday
     return render_template("Register Voter.html")
 
