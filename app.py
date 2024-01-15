b80389409dab9ef9bc11945f313256e794d704a8
LukeYaxley <lukeyaxley21@gmail.com>
2024-01-15 12:43:16 +0000

    Added Officer Dashboard and mechanism to stop users from voting >1

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def login():  # put application's code here
                 officers = f.read().splitlines()
                 if email in officers:
                     session['Officer'] = True
-                    return redirect(url_for("ODashboard"))
+                    return redirect(url_for("Officer_Dashboard"))
             return redirect(url_for("VDashboard"))
         #catch firebase errors for if login doesn't work
         except requests.exceptions.HTTPError as e:
@@ -60,7 +60,9 @@ def logout():
     redirect('/')
 @app.route('/register', methods =["GET", "POST"])
 def register():
+    error = None
     if request.method == "POST":
+
         # getting input with name = UVC in HTML form
         UVC = request.form.get("UVC")
         Name = request.form.get("Name")
@@ -68,8 +70,9 @@ def register():
         Birthday = request.form.get("Birthday")
         VoterID = request.form.get("VoterID")
         Password = request.form.get("Password")
-        jsonfile = {"Voters/"+UVC:{"Name":Name,"Const":Const,"Birthday":Birthday,"VoterID":VoterID}}
+        jsonfile = {"Voters/"+UVC:{"Name":Name,"Const":Const,"Birthday":Birthday,"VoterID":VoterID,"Vote":True}}
         if Check.validUVC(UVC) == True:
+
             #create user in firebase
             try:
                 auth.create_user_with_email_and_password(VoterID,Password)
@@ -97,13 +100,17 @@ def VDashboard():
         email = session['User']
     except KeyError:
         return redirect('/')
-    voters = db.child("Voters").order_by_child("VoterID").equal_to(email).get().val()
-    Const = next(iter(voters.values()))['Const']
-    Candidates = db.child('Candidates').get().val()
-    Constituency_data = Candidates[int(Const)][1:]
-    session['Const']= Const
-    return render_template("Voter Dashboard.html",Constituency_data = Constituency_data)
-
+    if db.child("Open").get().val() == True:
+        voters = db.child("Voters").order_by_child("VoterID").equal_to(email).get().val()
+        if next(iter(voters.values()))['Vote'] == False:
+            return render_template("Thank You.html")
+        Const = next(iter(voters.values()))['Const']
+        Candidates = db.child('Candidates').get().val()
+        Constituency_data = Candidates[int(Const)][1:]
+        session['Const']= Const
+        return render_template("Voter Dashboard.html",Constituency_data = Constituency_data)
+    else:
+        return render_template("Voting Closed.html")
 @app.route('/ThankYou',methods =["GET", "POST"])
 def submit_vote():
     if request.method == "POST":
@@ -112,13 +119,29 @@ def submit_vote():
         candidates = db.child("Candidates").child(int(selected_candidate[0]))
         Votes = candidates.get().val()[1:][0]["Votes"]
         db.child("Candidates").child(session['Const']).child(int(selected_candidate[0])).update({"Votes":(Votes+1)})
-        #db.child("Candidates").child(int(selected_candidate[0])).update()
 
+        voters = db.child("Voters").order_by_child("VoterID").equal_to(session['User'])
+        UVC = next(iter(voters.get().val().keys())) #get UVC
+        db.child("Voters").child(UVC).update({"Vote":False})
     return render_template("Thank You.html")
 
-@app.route("/ODashboard")
+@app.route("/ODashboard",methods = ["GET","POST"])
 def Officer_Dashboard():
-    return render_template("Officer Dashboard.html")
+    initial_checkbox = False
+    try:
+        email = session['User']
+    except KeyError:
+        return redirect('/')
+    if request.method == "GET":
+        if db.child("Open").get().val() == True:
+            initial_checkbox = True
 
+    if request.method == "POST":
+        if request.form.get("Voting") == "on":
+            db.update({"Open":True})
+        else:
+            db.update({"Open": False})
+    return render_template("Officer Dashboard.html",initial_checkbox_value=initial_checkbox)
+    # show voting results
 if __name__ == '__main__':
     app.run()
